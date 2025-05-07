a1fdf12909b632ba46b05b57b399a6482c48d0fa
tetrisTESTER22 <niepielievdaniil@gmail.com>
2025-05-07 13:32:58 +0300

    restart of the project

---
+++ mobile-bank-app/src/components/accountCard/AccountCard.tsx
@@ -0,0 +1,12 @@
+// components/AccountCard.tsx
+import './AccountCard.css'
+
+export default function AccountCard() {
+  return (
+    <div className="account-card">
+      <p className="account-title">FOLLOWS EJS</p>
+      <p className="account-details">Select Account · 85710350 · 55-70-13</p>
+      <p className="account-balance">£0.00</p>
+    </div>
+  )
+}
